b59d278c421cce33fad098b1cfe44f0e80029e0d
srikanth20k <srikanthreddya345@gmail.com>
2026-02-04 12:20:36 +0530

    adding file from local to git repo

---
+++ testing/oparations_test.py
@@ -0,0 +1,12 @@
+from src.oparations import add,sub
+
+
+def test_add(a,b):
+    assert (1+2)==3
+    assert (0+1)==1
+    assert (7+2)==9
+    
+def test_sub(a,b):
+    assert (1-2)==-1
+    assert (3-1)==2
+    assert (4-2)==2
